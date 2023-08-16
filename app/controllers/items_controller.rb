94da34e5f1f54fe499a159f4a18ba185752f478a
donvine mugendi <donvinemugendi@gmail.com>
2023-08-16 20:55:32 +0300

    completed lab

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,12 @@
 class ItemsController < ApplicationController
+    def index 
+        render json: Item.all.as_json(
+          only: [ :id, :name, :price, :description],
+          include: {
+            user: {
+              only: [:id, :username, :city,]
+            }
+          }
+        ), status: :ok
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,13 @@
 class UsersController < ApplicationController
+    def show 
+        user = User.find_by(id: params[:id])
+        render json: user.as_json(
+          only: [:id, :username, :city],
+          include: {
+            items: {
+              only: [:id, :name, :price, :description]
+            }
+          }
+        ), status: :ok
+    end
 end
